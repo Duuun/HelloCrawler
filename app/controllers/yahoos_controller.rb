06e13576c6766525855ac395be00d39d3c82347d
Daisuke Murakami <daimura10@gmail.com>
2018-03-21 20:59:23 +0900

    add text

--- app/controllers/yahoos_controller.rb
+++ app/controllers/yahoos_controller.rb
@@ -3,8 +3,8 @@ class YahoosController < ApplicationController
   require "nokogiri"
 
   def index
-    uri = "http://www.hatena.ne.jp/hotentry/"
-    @doc = Nokogiri::HTML(open(uri), nil, "utf-8")
-    # @doc = Nokogiri::HTML(open(uri), nil, "Shift-jis")
+    uri = "http://anime-antena.com/precure.html/"
+    # @doc = Nokogiri::HTML(open(uri), nil, "utf-8")
+    @doc = Nokogiri::HTML(open(uri), nil, "Shift-jis")
   end
 end
